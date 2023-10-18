fe88b569177da1086c63fd84ea0e5fca0727d3bb
mfilimonov <m.filimonov@finhike.com>
2023-10-18 19:58:23 +0300

    initial commit

---
+++ src/main/java/com/example/purerestapi/PureRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.purerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PureRestApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PureRestApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/purerestapi/PureRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.purerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PureRestApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
